dc6d00e1143db5bb4338dd137bdd14a05ecd490a
nenadV91 <nenadwebdevelop@gmail.com>
2023-06-27 14:22:46 +0200

    feat: update list generation code

--- src/scripts/staticList.js
+++ src/scripts/staticList.js
@@ -8,16 +8,22 @@ const coinGeckoListUrl = "https://files.cow.fi/tokens/CoinGecko.json";
 const coinGeckoIdListUrl = "https://api.coingecko.com/api/v3/coins/list";
 
 const LIST_DIR = path.join("src", "lists");
-const IDS_FILE_NAME = "id-list.json";
-const IDS_FILE_PATH = path.join(LIST_DIR, IDS_FILE_NAME);
 
-const STATIC_LIST_NAME = "static-list.json";
-const STATIC_LIST_PATH = path.join(LIST_DIR, STATIC_LIST_NAME);
+const IDS_FILE_NAME_RAW = "id-list-raw.json";
+const IDS_FILE_PATH_RAW = path.join(LIST_DIR, IDS_FILE_NAME_RAW);
 
-const STATIC_LIST_PARSED_NAME = "static-list-parsed.json";
-const STATIC_LIST_PARSED_PATH = path.join(LIST_DIR, STATIC_LIST_NAME);
+const IDS_FILE_NAME_FINAL = "id-list-final.json";
+const IDS_FILE_PATH_FINAL = path.join(LIST_DIR, IDS_FILE_NAME_FINAL);
+
+const STATIC_LIST_NAME_RAW = "static-list-raw.json";
+const STATIC_LIST_PATH_RAW = path.join(LIST_DIR, STATIC_LIST_NAME_RAW);
+
+const STATIC_LIST_NAME_FINAL = "static-list-final.json";
+const STATIC_LIST_PATH_FINAL = path.join(LIST_DIR, STATIC_LIST_NAME_FINAL);
 
 const CUSTOM_DESCRIPTION_PATH = path.join("src", "files", "description.json");
+
+const TOTAL_LIST_LENGTH = 50;
 
 async function fetchFromCoingecko(url) {
   console.log(`Fetching ${url}`);
@@ -35,7 +41,7 @@ async function writeFile(dir, filename, input) {
   const data = JSON.stringify(input, null, 4);
   const filePath = path.join(dir, filename);
 
-  console.log(`Writing file ${filePath}`);
+  console.log(`Writing file ${filePath}, length: ${input.length}`);
 
   if (!fs.existsSync(dir)) {
     fs.mkdirSync(dir, { recursive: true });
@@ -51,48 +57,54 @@ function sleep(ms) {
 async function getTokenLists() {
   // Get token lists
   const cowList = await fetchFromCoingecko(cowListUrl);
-  const coinGeckoList = await fetchFromCoingecko(coinGeckoListUrl);
+  // const coinGeckoList = await fetchFromCoingecko(coinGeckoListUrl);
 
-  // Combine 2 lists
-  const combined = [...cowList.tokens, ...coinGeckoList.tokens];
+  // Create input token list
+  // const result = [...cowList.tokens, ...coinGeckoList.tokens];
+  const list = [...cowList.tokens];
 
-  console.log("Combined tokens", combined.length);
+  // Make the list unique by symbol
+  const unique = _.uniqBy(list, ({ symbol }) => symbol);
 
-  return combined;
+  // Get an address and lowercase symbols
+  const result = unique.map(({ symbol, address }) => ({
+    symbol: symbol.toLowerCase(),
+    address: address.toLowerCase(),
+  }));
+
+  console.log("Input token list", result.length);
+
+  return result;
 }
 
 async function getIds() {
   // If ID's file exist
-  if (fs.existsSync(IDS_FILE_PATH)) {
-    const json = fs.readFileSync(IDS_FILE_PATH, "utf8");
+  if (fs.existsSync(IDS_FILE_PATH_RAW)) {
+    const json = fs.readFileSync(IDS_FILE_PATH_RAW, "utf8");
     return JSON.parse(json);
   }
 
-  const combined = await getTokenLists();
+  // Get input list
+  const inputList = await getTokenLists();
 
   // Get Coingecko id list
   const coinGeckoIdList = await fetchFromCoingecko(coinGeckoIdListUrl);
 
-  // Crate unique list by symbols from those 2
-  const uniq = _.uniqBy(combined, ({ symbol }) => symbol);
-  // Get just an array of symbols
-  const symbols = uniq.map(({ symbol }) => symbol.toLowerCase());
-
   // Get coin gecko list of ids for that uniq list
   const idsData = coinGeckoIdList.filter((el) =>
-    symbols.includes(el.symbol.toLowerCase())
+    inputList.some((input) => input.symbol === el.symbol.toLowerCase())
   );
 
   // Write ID's file
-  writeFile(LIST_DIR, IDS_FILE_NAME, idsData);
+  writeFile(LIST_DIR, IDS_FILE_NAME_RAW, idsData);
 
   return idsData;
 }
 
 async function getStaticData(idsData) {
   // If static data file exists, read and return it
-  if (fs.existsSync(STATIC_LIST_PATH)) {
-    const json = fs.readFileSync(STATIC_LIST_PATH, "utf8");
+  if (fs.existsSync(STATIC_LIST_PATH_RAW)) {
+    const json = fs.readFileSync(STATIC_LIST_PATH_RAW, "utf8");
     return JSON.parse(json);
   }
 
@@ -122,12 +134,12 @@ async function getStaticData(idsData) {
   }
 
   // Write static data file
-  writeFile(LIST_DIR, STATIC_LIST_NAME, staticData);
+  writeFile(LIST_DIR, STATIC_LIST_NAME_RAW, staticData);
 
   return staticData;
 }
 
-async function parseStaticData(data) {
+async function getStaticDataFinal(data) {
   // Get only USD values for market data
   let customDescriptionFile = null;
 
@@ -136,10 +148,11 @@ async function parseStaticData(data) {
     customDescriptionFile = JSON.parse(json);
   }
 
+  // Modify the data
   const parsed = data.map((item) => {
     const output = { ...item };
 
-    // Market data
+    // Parse Market data to only get USD values
     const market_data = output.market_data;
 
     for (let [key, value] of Object.entries(market_data)) {
@@ -149,7 +162,7 @@ async function parseStaticData(data) {
     }
     output.market_data = market_data;
 
-    // Description
+    // Add custom description
     if (customDescriptionFile && customDescriptionFile[item.id]) {
       output.description = customDescriptionFile[item.id];
     }
@@ -157,7 +170,7 @@ async function parseStaticData(data) {
     return output;
   });
 
-  // Get hash map (object) of token IDS from our 2 existing lists
+  // Get hash map (object) of token IDS from our input list
   const combined = await getTokenLists();
   const combined_ids = combined.reduce((acc, { address }) => {
     acc[address.toLowerCase()] = true;
@@ -173,17 +186,38 @@ async function parseStaticData(data) {
     );
   });
 
-  writeFile(LIST_DIR, STATIC_LIST_PARSED_NAME, filtered);
+  // Sort the data by market cap and take limit the list
+  const output = filtered.slice(0, TOTAL_LIST_LENGTH);
 
-  return filtered;
+  writeFile(LIST_DIR, STATIC_LIST_NAME_FINAL, output);
+
+  return output;
+}
+
+async function getIdsFinal(data) {
+  const ids = data.map(({ name, symbol, address, id }) => ({
+    id,
+    name,
+    symbol,
+    address,
+  }));
+
+  writeFile(LIST_DIR, IDS_FILE_NAME_FINAL, ids);
+
+  return ids;
 }
 
 async function main() {
-  const idsData = await getIds();
-  const staticData = await getStaticData(idsData);
-  const parsedData = await parseStaticData(staticData);
+  if (!fs.existsSync(LIST_DIR)) {
+    fs.mkdirSync(LIST_DIR);
+  }
 
-  console.log("Final output tokens", parsedData.length);
+  const idsDataRaw = await getIds();
+  const staticDataRaw = await getStaticData(idsDataRaw);
+  const staticDataFinal = await getStaticDataFinal(staticDataRaw);
+  const idsDataFinal = await getIdsFinal(staticDataFinal);
+
+  console.log("Final output token length", idsDataFinal.length);
   console.log("Done!!!");
 }
 
