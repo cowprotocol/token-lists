91450f4960490dfe856d657220cefc23e7bbd645
Alfetopito <alfetopito@users.noreply.github.com>
2023-10-19 16:00:18 +0100

    feat: add permit info schema unittests

---
+++ src/permitInfo/permitInfo.schema.test.js
@@ -0,0 +1,161 @@
+import {describe, it} from 'node:test'
+import assert from 'node:assert'
+import Ajv from 'ajv'
+
+import schema from './permitInfo.schema.json' assert {type: 'json'}
+
+
+describe('The permitInfo schema', () => {
+  it('should be valid', () => {
+    const ajv = new Ajv()
+
+    assert.strictEqual(ajv.validateSchema(schema), true)
+  })
+})
+
+describe('Valid PermitInfo data', () => {
+
+  it('should be valid with `false` value', () => {
+    const data = {
+      '0x0000000000000000000000000000000000000000': false
+    }
+
+    const ajv = new Ajv()
+    const result = ajv.validate(schema, data)
+
+    assert.strictEqual(ajv.errors, null)
+    assert.strictEqual(result, true)
+  })
+
+  it('should be valid with `eip-2612` and no version', () => {
+    const data = {
+      '0x0000000000000000000000000000000000000000': {
+        type: 'eip-2612'
+      }
+    }
+
+    const ajv = new Ajv()
+    const result = ajv.validate(schema, data)
+
+    assert.strictEqual(ajv.errors, null)
+    assert.strictEqual(result, true)
+  })
+
+  it('should be valid with `eip-2612` and version', () => {
+    const data = {
+      '0x0000000000000000000000000000000000000000': {
+        type: 'eip-2612',
+        version: '1',
+      }
+    }
+
+    const ajv = new Ajv()
+    const result = ajv.validate(schema, data)
+
+    assert.strictEqual(ajv.errors, null)
+    assert.strictEqual(result, true)
+  })
+
+  it('should be valid with `dai-like` and no version', () => {
+    const data = {
+      '0x0000000000000000000000000000000000000000': {
+        type: 'dai-like'
+      }
+    }
+
+    const ajv = new Ajv()
+    const result = ajv.validate(schema, data)
+
+    assert.strictEqual(ajv.errors, null)
+    assert.strictEqual(result, true)
+  })
+
+  it('should be valid with `dai-like` and version', () => {
+    const data = {
+      '0x0000000000000000000000000000000000000000': {
+        type: 'dai-like',
+        version: "2"
+      }
+    }
+
+    const ajv = new Ajv()
+    const result = ajv.validate(schema, data)
+
+    assert.strictEqual(ajv.errors, null)
+    assert.strictEqual(result, true)
+  })
+})
+
+describe('Invalid PermitInfo data', () => {
+  it('should be invalid with number version', () => {
+    const data = {
+      '0x0000000000000000000000000000000000000000': {
+        type: 'eip-2612',
+        version: 1,
+      }
+    }
+
+    const ajv = new Ajv()
+    const result = ajv.validate(schema, data)
+
+    assert.strictEqual(result, false)
+    assert.notEqual(ajv.errors, null)
+  })
+
+  it('should be invalid with non integer version', () => {
+    const data = {
+      '0x0000000000000000000000000000000000000000': {
+        type: 'eip-2612',
+        version: '1.1'
+      }
+    }
+
+    const ajv = new Ajv()
+    const result = ajv.validate(schema, data)
+
+    assert.strictEqual(result, false)
+    assert.notEqual(ajv.errors, null)
+  })
+
+  it('should be invalid with non address key', () => {
+    const data = {
+      'not an address': false
+    }
+
+    const ajv = new Ajv()
+    const result = ajv.validate(schema, data)
+
+
+    assert.strictEqual(result, false)
+    assert.notEqual(ajv.errors, null)
+  })
+
+  it('should be invalid with `true` value', () => {
+
+    const data = {
+      '0x0000000000000000000000000000000000000000': true
+    }
+
+    const ajv = new Ajv()
+    const result = ajv.validate(schema, data)
+
+    assert.strictEqual(result, false)
+    assert.notEqual(ajv.errors, null)
+  })
+
+  it('should be invalid with non existent type', () => {
+
+    const data = {
+      '0x0000000000000000000000000000000000000000': {
+        type: 'non-existent'
+      }
+    }
+
+    const ajv = new Ajv()
+    const result = ajv.validate(schema, data)
+
+    assert.strictEqual(result, false)
+    assert.notEqual(ajv.errors, null)
+  })
+
+})
