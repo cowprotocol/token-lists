a46968abd8ac235850216d616b31ad84283697be
Alfetopito <alfetopito@users.noreply.github.com>
2023-11-02 14:48:04 +0000

    chore: add comment regarding const address

--- src/permitInfo/const.ts
+++ src/permitInfo/const.ts
@@ -1,6 +1,7 @@
 import { env } from 'node:process'
 import { join } from 'node:path'
 
+// CoW protocol contract address. Could be any address in theory for checking the token is permittable
 export const SPENDER_ADDRESS = '0xC92E8bdf79f0507f65a392b0ab4667716BFE0110'
 
 export const DEFAULT_RPC_URLS: Record<number, string> = {
