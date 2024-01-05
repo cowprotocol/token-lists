7aad90f729444aaaad216b61e706a1bc4c85fda5
shoom3301 <shoom3301@gmail.com>
2024-01-05 14:54:16 +0600

    chore: update sepolia permit

--- src/permitInfo/utils/getTokensFromTokenList.ts
+++ src/permitInfo/utils/getTokensFromTokenList.ts
@@ -3,10 +3,17 @@ import { BASE_PATH } from '../const.ts'
 import { Token } from '../types.ts'
 import { join } from 'node:path'
 
+const tokenListsByNetwork: Record<number, string> = {
+  1: 'CowSwap.json',
+  100: 'CowSwap.json',
+  5: 'CowSwapGoerli.json',
+  11155111: 'CowSwapSepolia.json',
+}
+
 export function getTokensFromTokenList(chainId: number, tokenListPath: string | undefined): Array<Token> {
   const filePath = tokenListPath
     ? tokenListPath
-    : join(BASE_PATH, chainId === 5 ? 'CowSwapGoerli.json' : 'CowSwap.json')
+    : join(BASE_PATH, tokenListsByNetwork[chainId])
 
   return JSON.parse(readFileSync(filePath, 'utf-8')).tokens
 }
