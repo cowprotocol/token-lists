bab7d191ebe38b73c046a67d71c214f1795ee00d
Alfetopito <alfetopito@users.noreply.github.com>
2024-05-24 16:41:30 +0100

    fix: overwrite bridge addresses when there are known addresses which differ

--- src/scripts/generateBridgeList.ts
+++ src/scripts/generateBridgeList.ts
@@ -13,6 +13,15 @@ type Call = {
   callData: string
 }
 
+type Extensions = {
+  bridgeInfo?: Record<
+    SupportedChainId,
+    {
+      tokenAddress?: string
+    }
+  >
+}
+
 async function generateBridgedList(
   chainId: SupportedChainId,
   tokenListSource: string | TokenList,
@@ -20,6 +29,7 @@ async function generateBridgedList(
   bridgeContractAbi: ContractInterface,
   methodName: string,
   outputFilePath: string,
+  tokensToReplace: Record<string, string | null> = {},
 ): Promise<void> {
   const provider = getProvider(chainId, undefined)
   const bridgeContract = new Contract(bridgeContractAddress, bridgeContractAbi, provider)
@@ -33,6 +43,8 @@ async function generateBridgedList(
   const calls = tokensList.tokens.reduce<{ token: TokenInfo; call: Call }[]>((acc, token) => {
     // Take only Mainnet tokens
     if (token.chainId !== SupportedChainId.MAINNET) return acc
+    // Do not check for tokens we want to skip/replace
+    if (tokensToReplace[token.address.toLowerCase()] === null) return acc
 
     // A call to resolve token address in target chain
     const call = {
@@ -60,9 +72,45 @@ async function generateBridgedList(
         // Get rid of tokens that are not known by the bridge contract
         if (!token || address === ZERO_ADDRESS) return acc
 
+        const toReplace = tokensToReplace[token.address.toLowerCase()]
+        if (toReplace !== undefined) {
+          if (toReplace === null) {
+            // Do not add to the list
+            return acc
+          }
+
+          // Replace the fetched address with the one provided
+          acc.push({
+            ...token,
+            chainId,
+            address: toReplace,
+            extensions: undefined,
+          })
+
+          return acc
+        }
         // Copy everything from the Mainnet token and override address and chainId
         // Let's hope no one deploys the same token in a different networks with different data
 
+        const extensions = token.extensions as Extensions
+        const tokenAddress = extensions?.bridgeInfo?.[chainId]?.tokenAddress
+
+        if (tokenAddress && tokenAddress.toLowerCase() !== address.toLowerCase()) {
+          console.log(
+            `TokenList extension contains address different than returned by bridge. Extension: ${tokenAddress} Bridge: ${address}`,
+          )
+
+          acc.push({
+            ...token,
+            chainId,
+            address: tokenAddress,
+            extensions: undefined,
+          })
+
+          return acc
+        }
+
+        // Use bridge address
         acc.push({
           ...token,
           chainId,
@@ -102,5 +150,11 @@ export async function generateArbitrumOneChainList(source: string | TokenList, r
     ARBITRUM_BRIDGE_ABI,
     'calculateL2TokenAddress',
     resultFile,
+    {
+      // USDC
+      '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48': '0xaf88d065e77c8cc2239327c5edb3a432268e5831',
+      // WETH
+      '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2': '0x82af49447d8a07e3bd95bd0d56f35241523fbab1',
+    },
   )
 }
