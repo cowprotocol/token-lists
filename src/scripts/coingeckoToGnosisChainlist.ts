88198abf73a770e54d9c6329ba0f76e00d3b2a4a
Alfetopito <alfetopito@users.noreply.github.com>
2024-05-24 14:46:30 +0100

    chore: remove coingeckoToGnosisChainList script

--- src/scripts/coingeckoToGnosisChainlist.ts
+++
@@ -1,6 +0,0 @@
-import { generateGnosisChainList } from './generateGnosisChainList'
-import sourceList from '../public/CoinGecko.json' assert { type: "json" }
-
-const TOKENS_LIST_FILE_NAME = 'GnosisCoingeckoTokensList.json'
-
-generateGnosisChainList(sourceList, TOKENS_LIST_FILE_NAME).catch(console.error)
