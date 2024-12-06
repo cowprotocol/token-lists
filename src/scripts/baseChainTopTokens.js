88d433e341bc5b31bd12a6d955303165b2f4122a
Alfetopito <alfetopito@users.noreply.github.com>
2024-12-06 16:09:58 +0000

    feat: sooo many changes

--- src/scripts/baseChainTopTokens.js
+++ src/scripts/baseChainTopTokens.js
@@ -5,76 +5,224 @@ import path from 'path'
 
 const TOP_TOKENS_COUNT = 500 // Number of top tokens to display
 
-const TOKEN_LIST_URL = 'https://tokens.coingecko.com/base/all.json'
-const COINGECKO_API_URL = 'https://pro-api.coingecko.com/api/v3/coins/base/contract'
 const VS_CURRENCY = 'usd' // Base currency for volume data
 
-const TOKEN_LIST_INFO = {
-  name: 'CoinGecko',
-  logoURI:
-    'https://static.coingecko.com/s/thumbnail-007177f3eca19695592f0b8b0eabbdae282b54154e1be912285c9034ea6cbaf2.png',
-  keywords: ['defi'],
-  version: {
-    major: 0,
-    minor: 0,
-    patch: 0,
-  },
+let COINGECKO_IDS_MAP = {}
+
+const COINGECKO_CHAINS = {
+  1: 'ethereum',
+  100: 'xdai',
+  8453: 'base',
+  42161: 'arbitrum-one',
 }
+const DISPLAY_CHAIN_NAMES = {
+  1: 'Ethereum',
+  100: 'Gnosis chain',
+  8453: 'Base',
+  42161: 'Arbitrum one',
+}
+const COINGECKO_CHAINS_NAMES = Object.values(COINGECKO_CHAINS)
 
 const COINGECKO_API_KEY = process.env.COINGECKO_API_KEY
 assert(COINGECKO_API_KEY, 'COINGECKO_API_KEY env is required')
 
-async function getTokenList() {
-  const response = await fetch(TOKEN_LIST_URL)
+function getCoingeckoApiUrl(chain) {
+  return `https://pro-api.coingecko.com/api/v3/coins/${DISPLAY_CHAIN_NAMES[chain]}/contract`
+}
+
+function getTokenListUrl(chain) {
+  return `https://tokens.coingecko.com/${COINGECKO_CHAINS[chain]}/all.json`
+}
+
+function getEmptyList(chain) {
+  return {
+    name: `Coingecko top ${TOP_TOKENS_COUNT} on ${COINGECKO_CHAINS[chain]}`,
+    logoURI:
+      'https://static.coingecko.com/s/thumbnail-007177f3eca19695592f0b8b0eabbdae282b54154e1be912285c9034ea6cbaf2.png',
+    keywords: ['defi'],
+    version: {
+      major: 0,
+      minor: 0,
+      patch: 0,
+    },
+  }
+}
+
+async function getTokenList(chain) {
+  const tokenListUrl = getTokenListUrl(chain)
+  const response = await fetch(tokenListUrl)
   const data = await response.json()
   return data.tokens
 }
 
-async function getTokenVolume(token) {
+async function getTokenVolume(token, chain) {
   try {
     const headers = COINGECKO_API_KEY ? { 'X-Cg-Pro-Api-Key': COINGECKO_API_KEY } : {}
 
-    const url = `${COINGECKO_API_URL}/${token.address}?localization=false`
+    const url = `${getCoingeckoApiUrl(chain)}/${token.address}?localization=false`
     const response = await fetch(url, { headers })
     const data = await response.json()
 
-    // console.log('[getTokenVolume] url:', url)
-    // console.log('[getTokenVolume] token:', token.symbol)
-    // console.log(`[getTokenVolume] ${token.symbol} $${data.market_data.total_volume[VS_CURRENCY]}`)
+    // console.log(`[getTokenVolume] url: ${url}, chain: ${chain}`)
+    // console.log(`[getTokenVolume] token: ${token.symbol}, chain: ${chain}`)
+    // console.log(`[getTokenVolume] ${token.symbol} $${data.market_data.total_volume[VS_CURRENCY]}, chain: ${chain}`)
     return data.market_data.total_volume[VS_CURRENCY] || 0
   } catch (error) {
-    console.error(`Error fetching volume for token ${token.name}:`, error)
+    console.error(`Error fetching volume for token ${token.name} on chain ${chain}:`, error)
     return 0
   }
 }
 
-async function main() {
+async function getCoingeckoTokenIds() {
   try {
+    const headers = COINGECKO_API_KEY ? { 'X-Cg-Pro-Api-Key': COINGECKO_API_KEY } : {}
+
+    const url = `https://pro-api.coingecko.com/api/v3/coins/list?include_platform=true&status=active`
+    const response = await fetch(url, { headers })
+    return await response.json()
+  } catch (error) {
+    console.error(`Error fetching Coingecko's coin list`, error)
+    return []
+  }
+}
+
+/**
+ * Get a map of contract addresses to coingecko coin id.
+ *
+ * Result is stored in a local file since this shouldn't change often.
+ *
+ * Takes in one parameter forceUpdate, to ignore local content - if any.
+ */
+async function getCoingeckoTokenIdsMap(forceUpdate) {
+  const localFilePath = 'build/coingeckoTokenIdsMap.json'
+  let tokenIdsMap = COINGECKO_CHAINS_NAMES.reduce((acc, name) => ({ ...acc, [name]: {} }), {})
+
+  // Check if local file exists and forceUpdate is false
+  if (!forceUpdate && fs.existsSync(localFilePath)) {
+    try {
+      // Read the local file
+      const data = fs.readFileSync(localFilePath)
+      tokenIdsMap = JSON.parse(data)
+    } catch (error) {
+      console.error(`Error reading local file: ${error}`)
+    }
+  } else {
+    // Fetch the token IDs from Coingecko API
+    try {
+      const tokenIds = await getCoingeckoTokenIds()
+
+      tokenIds.forEach((token) => {
+        COINGECKO_CHAINS_NAMES.forEach((chain) => {
+          const address = token.platforms[chain]
+          if (address) {
+            tokenIdsMap[chain][address] = token.id
+            // reverse mapping
+            tokenIdsMap[chain][token.id] = address
+          }
+        })
+      })
+
+      // Save the fetched data to the local file
+      fs.writeFileSync(localFilePath, JSON.stringify(tokenIdsMap))
+    } catch (error) {
+      console.error(`Error fetching Coingecko token IDs: ${error}`)
+    }
+  }
+
+  return tokenIdsMap
+}
+
+async function getCoingeckoMarket(chainId, tokens) {
+  const coingeckoIdsForChain = COINGECKO_IDS_MAP[COINGECKO_CHAINS[chainId]]
+  const ids = tokens.reduce((acc, token) => {
+    const coingeckoId = coingeckoIdsForChain[token.address]
+    if (coingeckoId) {
+      acc += `${coingeckoId},`
+    }
+    return acc
+  }, '')
+
+  try {
+    const headers = COINGECKO_API_KEY ? { 'X-Cg-Pro-Api-Key': COINGECKO_API_KEY } : {}
+
+    const url = `https://pro-api.coingecko.com/api/v3/coins/markets?vs_currency=${VS_CURRENCY}&per_page=250&ids=${ids}`
+    const response = await fetch(url, { headers })
+    return await response.json()
+  } catch (error) {
+    console.error(`Error fetching Coingecko's coin list`, error)
+    return []
+  }
+}
+
+async function getTokenVolumes(chainId, tokens) {
+  // Max items per query is 250
+  const chunkSize = 250
+  const chunkVolumes = []
+
+  // Chunk tokens for each individual query
+  for (let i = 0; i < tokens.length; i += chunkSize) {
+    const chunk = tokens.slice(i, i + chunkSize)
+    chunkVolumes.push({ tokens: chunk, volume: getCoingeckoMarket(chainId, chunk) })
+  }
+
+  const ids = COINGECKO_IDS_MAP[COINGECKO_CHAINS[chainId]]
+
+  const volumes = await Promise.all(
+    chunkVolumes.map(async ({ tokens, volume }) => {
+      // Await query in parallel
+      const volumeData = await volume
+      // Create a map with volumes since the ordering is lost
+      const volumeMap = volumeData.reduce((acc, cur) => {
+        if (cur.total_volume && ids[cur.id]) {
+          acc[ids[cur.id]] = cur.total_volume || 0
+        }
+        return acc
+      }, {})
+
+      return tokens.reduce((acc, token) => {
+        if (volumeMap[token.address]) {
+          acc.push({ token, volume: volumeMap[token.address] })
+        }
+        return acc
+      }, [])
+    }),
+  )
+
+  // Flatten and sort volumes by volume in descending order
+  return volumes.flat().sort((a, b) => b.volume - a.volume)
+}
+
+async function fetchAndProcessTokens(chainId) {
+  try {
+    // Get coingecko ids
+    COINGECKO_IDS_MAP = await getCoingeckoTokenIdsMap()
+
     // Fetch tokens
-    const tokens = await getTokenList()
+    const tokens = await getTokenList(chainId)
 
     // Fetch volume for each token
-    const tokenVolumes = await Promise.all(
-      tokens.map(async (token) => ({
-        token,
-        volume: await getTokenVolume(token),
-      })),
-    )
+    // const tokenVolumes = await Promise.all(
+    //   tokens.map(async (token) => ({
+    //     token,
+    //     volume: await getTokenVolume(token, chainId),
+    //   })),
+    // )
 
     // Sort by volume and pick top tokens
-    const topTokens = tokenVolumes.sort((a, b) => b.volume - a.volume).slice(0, TOP_TOKENS_COUNT)
+    // const topTokens = tokenVolumes.sort((a, b) => b.volume - a.volume).slice(0, TOP_TOKENS_COUNT)
+    const topTokens = (await getTokenVolumes(chainId, tokens)).slice(0, TOP_TOKENS_COUNT)
 
     // Print top tokens
     let position = 1
-    console.log(`🥇 Top ${TOP_TOKENS_COUNT} tokens`)
+    console.log(`🥇 Top ${TOP_TOKENS_COUNT} tokens on chain ${chainId}`)
     for (const { token, volume } of topTokens) {
       console.log(`\t-${position.toString().padStart(3, '0')}) ${token.name} (${token.symbol}): $${volume}`)
       position++
     }
 
-    const tokenListPath = path.join('src/public/CoinGecko.8453.json')
+    const tokenListPath = path.join(`src/public/CoinGecko.${chainId}.json`)
 
-    const tokenList = getLocalTokenList(tokenListPath, TOKEN_LIST_INFO)
+    const tokenList = getLocalTokenList(tokenListPath, getEmptyList(chainId))
 
     // Replace tokens
     tokenList.tokens = topTokens.map(({ token }) => token)
@@ -82,8 +230,14 @@ async function main() {
     // Write token file
     saveLocalTokenList(tokenListPath, tokenList)
   } catch (error) {
-    console.error('Error fetching data:', error)
+    console.error(`Error fetching data for chain ${chainId}:`, error)
   }
+}
+
+async function main() {
+  // Fetch tokens for all chains
+  Object.keys(COINGECKO_CHAINS).forEach((chain) => fetchAndProcessTokens(chain))
+  // fetchAndProcessTokens(100)
 }
 
 /**
