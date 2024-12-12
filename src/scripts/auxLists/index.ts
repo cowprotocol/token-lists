6d16e8067598c883f655257c8c5e4148641870b7
Alfetopito <alfetopito@users.noreply.github.com>
2024-12-12 17:47:18 +0000

    chore: tiny fix

--- src/scripts/auxLists/index.ts
+++ src/scripts/auxLists/index.ts
@@ -5,7 +5,7 @@ import { COINGECKO_CHAINS } from './utils'
 async function main(): Promise<void> {
   Object.keys(COINGECKO_CHAINS).forEach((chain) => fetchAndProcessCoingeckoTokens(Number(chain)))
   Object.keys(COINGECKO_CHAINS)
-    .filter((chain) => chain != '1') // No need to create a list for mainnet
+    .filter((chain) => chain !== '1') // No need to create a list for mainnet
     .forEach((chain) => fetchAndProcessUniswapTokens(Number(chain)))
 }
 
