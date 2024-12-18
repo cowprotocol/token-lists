d4f53bbda7ac907b8797b51a34489dd25595731c
Leandro <alfetopito@users.noreply.github.com>
2024-12-18 13:37:42 +0000

    feat: override aux list props (#768)
    * refactor: extract processTokenList into a new file
    
    * feat: add option to override token details
    
    * chore: update lists
    
    * fix: avoid issue where all values use the same object

--- src/scripts/auxLists/index.ts
+++ src/scripts/auxLists/index.ts
@@ -1,12 +1,16 @@
+import { mapSupportedNetworks, SupportedChainId } from '@cowprotocol/cow-sdk'
 import { fetchAndProcessCoingeckoTokens } from './coingecko'
 import { fetchAndProcessUniswapTokens } from './uniswap'
-import { getCoingeckoTokenIdsMap } from './utils'
+import { getCoingeckoTokenIdsMap, OverridesPerChain } from './utils'
+
+const OVERRIDES: OverridesPerChain = mapSupportedNetworks(() => ({}))
+OVERRIDES[SupportedChainId.BASE]['0x18dd5b087bca9920562aff7a0199b96b9230438b'] = { decimals: 8 } // incorrect decimals set on CoinGecko's list
 
 async function main(): Promise<void> {
   const COINGECKO_IDS_MAP = await getCoingeckoTokenIdsMap()
 
-  fetchAndProcessCoingeckoTokens(COINGECKO_IDS_MAP)
-  fetchAndProcessUniswapTokens(COINGECKO_IDS_MAP)
+  fetchAndProcessCoingeckoTokens(COINGECKO_IDS_MAP, OVERRIDES)
+  fetchAndProcessUniswapTokens(COINGECKO_IDS_MAP, OVERRIDES)
 }
 
 main()
