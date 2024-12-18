6b8466b620d7c3890f0133e4882659f4ac2c8a3d
Alfetopito <alfetopito@users.noreply.github.com>
2024-12-18 13:06:05 +0000

    fix: avoid issue where all values use the same object

--- src/scripts/auxLists/index.ts
+++ src/scripts/auxLists/index.ts
@@ -3,7 +3,7 @@ import { fetchAndProcessCoingeckoTokens } from './coingecko'
 import { fetchAndProcessUniswapTokens } from './uniswap'
 import { getCoingeckoTokenIdsMap, OverridesPerChain } from './utils'
 
-const OVERRIDES: OverridesPerChain = mapSupportedNetworks({})
+const OVERRIDES: OverridesPerChain = mapSupportedNetworks(() => ({}))
 OVERRIDES[SupportedChainId.BASE]['0x18dd5b087bca9920562aff7a0199b96b9230438b'] = { decimals: 8 } // incorrect decimals set on CoinGecko's list
 
 async function main(): Promise<void> {
