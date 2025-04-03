50a9e1f71222f684134c94f7fe01955dc78debcc
Leandro <alfetopito@users.noreply.github.com>
2025-04-03 11:58:33 +0100

    fix: overwrite wxDAI name & symbol coming from Coingecko (#886)

--- src/scripts/auxLists/index.ts
+++ src/scripts/auxLists/index.ts
@@ -6,6 +6,10 @@ import { getCoingeckoTokenIdsMap, OverridesPerChain } from './utils'
 const OVERRIDES: OverridesPerChain = mapSupportedNetworks(() => ({}))
 OVERRIDES[SupportedChainId.BASE]['0x18dd5b087bca9920562aff7a0199b96b9230438b'] = { decimals: 8 } // incorrect decimals set on CoinGecko's list
 OVERRIDES[SupportedChainId.BASE]['0xe231db5f348d709239ef1741ea30961b3b635a61'] = { decimals: 18 } // incorrect decimals set on CoinGecko's list
+OVERRIDES[SupportedChainId.GNOSIS_CHAIN]['0xe91d153e0b41518a2ce8dd3d7944fa863463a97d'] = {
+  symbol: 'wxDAI',
+  name: 'Wrapped xDAI',
+} // incorrect symbol and name set on CoinGecko's list
 
 async function main(): Promise<void> {
   const COINGECKO_IDS_MAP = await getCoingeckoTokenIdsMap()
