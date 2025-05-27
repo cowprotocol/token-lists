2feb7e3a55ba9d841e0ea56316a5884eabdc3f10
cowdan <daniele@cow.fi>
2025-05-27 13:29:14 +0100

    fix: `fetchPermitInfo` restore command line arguments (#941)

--- src/permitInfo/fetchPermitInfoByChain.ts
+++ src/permitInfo/fetchPermitInfoByChain.ts
@@ -1,28 +1,28 @@
-import path from "path"
+import path from 'path'
 import { argv, chdir, exit } from 'node:process'
 
 import { SupportedChainId } from '@cowprotocol/cow-sdk'
-import { fetchPermitInfo } from "./fetchPermitInfo"
+import { fetchPermitInfo } from './fetchPermitInfo'
 
 function fetchPermitInfoByChain() {
-    const [, scriptPath, chainId, forceRecheck] = argv
-  
-    if (!chainId) {
-      console.error('ChainId is missing. Invoke the script with the chainId as the first parameter.')
-      exit(1)
-    }
-  
-    // Change to script dir so relative paths work properly
-    chdir(path.dirname(scriptPath))
-  
-    // Execute the script
-    fetchPermitInfo({
-      chainId: +chainId as SupportedChainId,
-      tokenListPath: undefined,
-      rpcUrl: undefined,
-      recheckUnsupported: false,
-      forceRecheck: forceRecheck === 'true',
-    }).then(() => console.info(`Done 🏁`))
+  const [, scriptPath, chainId, tokenListPath, rpcUrl, recheckUnsupported, forceRecheck] = argv
+
+  if (!chainId) {
+    console.error('ChainId is missing. Invoke the script with the chainId as the first parameter.')
+    exit(1)
   }
-  
-  fetchPermitInfoByChain()
+
+  // Change to script dir so relative paths work properly
+  chdir(path.dirname(scriptPath))
+
+  // Execute the script
+  fetchPermitInfo({
+    chainId: +chainId as SupportedChainId,
+    tokenListPath,
+    rpcUrl,
+    recheckUnsupported: recheckUnsupported === 'true',
+    forceRecheck: forceRecheck === 'true',
+  }).then(() => console.info(`Done 🏁`))
+}
+
+fetchPermitInfoByChain()
