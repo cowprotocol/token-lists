069cc1882a2e0cd815b0cb8d1d20aa5702d89bfa
Alfetopito <alfetopito@users.noreply.github.com>
2025-10-24 18:40:27 +0100

    fix: update imports

--- src/scripts/install-sdk-preview.mjs
+++ src/scripts/install-sdk-preview.mjs
@@ -1,6 +1,5 @@
-const fs = require('fs')
-
-const packageJson = require('../../package.json')
+import fs from 'fs'
+import packageJson from '../../package.json' with { type: 'json' }
 
 export function installSdkPreview(context, core) {
   const sdkPrVersionRegex = /pr-\d+/
