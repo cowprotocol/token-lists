8e46e113f59329cf3ab35350957d446acbef91fc
Alfetopito <alfetopito@users.noreply.github.com>
2025-10-24 18:36:37 +0100

    fix: another attempt

--- src/scripts/install-sdk-preview.js
+++ src/scripts/install-sdk-preview.js
@@ -2,32 +2,35 @@ const fs = require('fs')
 
 const packageJson = require('../../package.json')
 
-const sdkPrVersionRegex = /pr-\d+/
+export default function installSdkPreview(context, core) {
+  const sdkPrVersionRegex = /pr-\d+/
 
-const sdkPrefix = '@cowprotocol/'
-const hasSdkPrVersion = Object.keys(packageJson.dependencies)
-  .filter((key) => key.startsWith(sdkPrefix))
-  .some((key) => {
-    const version = packageJson.dependencies[key]
+  const sdkPrefix = '@cowprotocol/'
+  const hasSdkPrVersion = Object.keys(packageJson.dependencies)
+    .filter((key) => key.startsWith(sdkPrefix))
+    .some((key) => {
+      const version = packageJson.dependencies[key]
 
-    return sdkPrVersionRegex.test(version)
-  })
+      return sdkPrVersionRegex.test(version)
+    })
 
-if (!hasSdkPrVersion) {
-  console.log('[install-sdk-preview.js] no SDK PR version set, skipping')
-  process.exit(0)
-}
+  if (!hasSdkPrVersion) {
+    console.log('[install-sdk-preview.js] no SDK PR version set, skipping')
+    core.setOutput('hasSdkPrVersion', 'false')
+    return
+  }
 
-const PACKAGE_READ_AUTH_TOKEN = process.env.PACKAGE_READ_AUTH_TOKEN
+  const PACKAGE_READ_AUTH_TOKEN = process.env.PACKAGE_READ_AUTH_TOKEN
 
-if (!PACKAGE_READ_AUTH_TOKEN) {
-  console.error(
-    '[install-sdk-preview.js] PACKAGE_READ_AUTH_TOKEN env var is not set but expected by install-sdk-preview.js',
-  )
-  process.exit(1)
-}
+  if (!PACKAGE_READ_AUTH_TOKEN) {
+    console.error(
+      '[install-sdk-preview.js] PACKAGE_READ_AUTH_TOKEN env var is not set but expected by install-sdk-preview.js',
+    )
+    core.setOutput('hasSdkPrVersion', 'false')
+    return
+  }
 
-const npmrc = `
+  const npmrc = `
 # Default: install everything else from public npm
 registry=https://registry.npmjs.org/
 
@@ -38,4 +41,6 @@ registry=https://registry.npmjs.org/
 //npm.pkg.github.com/:_authToken=${PACKAGE_READ_AUTH_TOKEN}
 `
 
-fs.writeFileSync('.npmrc', npmrc)
+  fs.writeFileSync('.npmrc', npmrc)
+  core.setOutput('hasSdkPrVersion', 'true')
+}
